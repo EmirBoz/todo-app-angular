66b3e7b44f5c2186b3441d205dedf14107e924be
Emir Boz <emirrbozz@gmail.com>
2021-12-22 21:17:48 +0300

    Model added

---
+++ src/app/model.ts
@@ -0,0 +1,25 @@
+export class Model {
+  user;
+  items;
+
+  constructor() {
+    this.user = 'Emir';
+    this.items = [
+                  new TodoItem('Sport',true),
+                  new TodoItem('Breakfast',false),
+                  new TodoItem('Cinema',false),
+                  new TodoItem('Homework',false)
+
+    ];
+  }
+}
+
+export class TodoItem {
+  description;
+  action;
+
+  constructor(description :string, action:boolean) {
+    this.description = description;
+    this.action = action;
+  }
+}
